108dcb7f44c75d89462223c4e855681c8ad09229
johanc.suarez <johanc.suarez@hotmail.com>
2020-02-08 09:39:07 -0500

    cambiado el texto de bienvenida

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 <title>Heroku prueba Johan 2020 </title>
 </head>
 <body>
-<?php
+<div align="center"><h1>Primera app empresariales 2020.</h1>
+<h3><b><?php
 echo 'Holaa ' . htmlspecialchars($_GET["name"]) . ' :)';
-?>
+?></h3></b></div>
 </body>
 </html>
